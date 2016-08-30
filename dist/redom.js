7896dba53e0e67ae4c1bd78b5e2b0a292d1016f2
Juha Lindstedt <juha@pakastin.fi>
2016-08-30 15:33:53 +0300

    fixed "constructor" call

--- dist/redom.js
+++ dist/redom.js
@@ -287,19 +287,19 @@ function view (proto) {
     var len = arguments.length;
 
     switch (len) {
-      case 0: proto.init(); break;
-      case 1: proto.init(a); break;
-      case 2: proto.init(a, b); break;
-      case 3: proto.init(a, b, c); break;
+      case 0: proto.init.call(view); break;
+      case 1: proto.init.call(view, a); break;
+      case 2: proto.init.call(view, a, b); break;
+      case 3: proto.init.call(view, a, b, c); break;
       default:
         var args = new Array(len);
         var i = 0;
         while (i < len) {
-          proto.init.apply(this, args);
+          proto.init.apply(view, args);
         }
       break;
     }
-    
+
     return view;
   }
 }
