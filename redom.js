8cae4e09fbcddf2f5daef281f8f06b8bb6f0ed68
Juha Lindstedt <juha@pakastin.fi>
2016-09-01 18:13:23 +0300

    Updates

--- redom.js
+++ redom.js
@@ -4,8 +4,133 @@
   (factory((global.redom = global.redom || {})));
 }(this, (function (exports) { 'use strict';
 
-function text (content) {
-  return document.createTextNode(content);
+function createElement (query, ns) {
+  // query parsing magic by https://github.com/maciejhirsz
+
+  var tag, id, className;
+
+  var mode = 0;
+  var start = 0;
+
+  for (var i = 0, len = query.length; i <= len; i++) {
+    var cp = i === len ? 0 : query.charCodeAt(i);
+
+    //  cp === '#'     cp === '.'     nullterm
+    if (cp === 0x23 || cp === 0x2E || cp === 0) {
+      if (mode === 0) {
+        tag = i  === 0 ? 'div'
+            : cp === 0 ? query
+            :            query.substring(start, i);
+      } else {
+        var slice = query.substring(start, i)
+        if (mode === 1) {
+          id = slice;
+        } else if (className) {
+          className += ' ' + slice;
+        } else {
+          className = slice;
+        }
+      }
+
+      start = i + 1;
+      mode = cp === 0x23 ? 1 : 2;
+    }
+  }
+  if (ns) {
+    var element = document.createElementNS(ns, tag);
+  } else {
+    var element = document.createElement(tag);
+  }
+
+  if (id) element.id = id;
+  if (className) element.className = className;
+
+  return element;
+}
+
+var cache = {};
+
+function el (query, a) {
+  if (typeof query === 'function') {
+    var len = arguments.length - 1;
+    if (len > 1) {
+      var args = new Array(len);
+      var i = 0;
+
+      while (i < len) args[++i] = arguments[i];
+
+      return new (query.bind.apply(query, args));
+    } else {
+      return new query(a);
+    }
+  }
+  var element = (cache[query] || (cache[query] = createElement(query))).cloneNode(false);
+  var empty = true;
+
+  for (var i = 1; i < arguments.length; i++) {
+    var arg = arguments[i];
+
+    while (typeof arg === 'function') {
+      arg = arg(element);
+    }
+
+    if (arg == null) {
+      continue;
+    }
+
+    if (arg.nodeType) {
+      element.appendChild(arg);
+    } else if (typeof arg === 'string' || typeof arg === 'number') {
+      if (empty) {
+        element.textContent = arg;
+      } else {
+        element.appendChild(document.createTextNode(arg));
+      }
+    } else if (arg.el && arg.el.nodeType) {
+      var child = arg;
+      var childEl = arg.el;
+
+      if (child !== childEl) {
+        child.el = childEl;
+        childEl.__redom_view = child;
+      }
+
+      if (child.isMounted) {
+        child.remounted && child.remounted();
+      } else {
+        child.mounted && child.mounted();
+      }
+
+      element.appendChild(childEl);
+    } else {
+      for (var key in arg) {
+        var value = arg[key];
+
+        if (key === 'style') {
+          if (typeof value === 'string') {
+            element.setAttribute(key, value);
+          } else {
+            for (var cssKey in value) {
+              element.style[cssKey] = value[cssKey];
+            }
+          }
+        } else if (key in element) {
+          element[key] = value;
+          if (key === 'autofocus') {
+            element.focus();
+          }
+        } else {
+          element.setAttribute(key, value);
+        }
+      }
+    }
+  }
+
+  return element;
+}
+
+el.extend = function (query) {
+  return el.bind(this, query);
 }
 
 function setChildren (parent, children) {
@@ -24,162 +149,207 @@ function setChildren (parent, children) {
       continue;
     }
 
-    mount(parent, child, traverse);
+    if (traverse) {
+      parentEl.insertBefore(childEl, traverse);
+    } else {
+      parentEl.appendChild(childEl);
+    }
   }
 
   while (traverse) {
     var next = traverse.nextSibling;
-    unmount(parent, traverse.view || traverse);
     traverse = next;
   }
 }
 
-function doMount (parent, child, before) {
-  if (before) {
-    parent.insertBefore(child, before.el || before);
-  } else {
-    parent.appendChild(child);
+function list (parent, View, key, initData) {
+  return new List(parent, View, key, initData);
+}
+
+list.extend = List.extend;
+
+function List(parent, View, key, initData) {
+  this.View = View;
+  this.key = key;
+  this.initData = initData;
+  this.views = [];
+  this.el = typeof parent === 'string' ? el(parent) : parent;
+
+  if (key) {
+    this.lookup = {};
   }
 }
 
+List.extend = function (parent, View, key, initData) {
+  return List.bind(List, parent, View, key, initData);
+}
+
+List.prototype.update = function (data) {
+  var View = this.View;
+  var key = this.key;
+  var initData = this.initData;
+  var views = this.views;
+  var parent = this.el;
+  var traverse = parent.firstChild;
+
+  if (key) {
+    var lookup = this.lookup;
+  }
+
+  for (var i = 0; i < data.length; i++) {
+    var item = data[i];
+    if (key) {
+      var id = typeof key === 'function' ? key(item) : item[key];
+      var view = views[i] = lookup[id] || (lookup[id] = new View(initData, item, i));
+      view.__id = id;
+    } else {
+      var view = views[i] || (views[i] = new View(initData, item, i));
+    }
+    var el = view.el;
+    view.el = el;
+    el.__redom_view = view;
+    view.update && view.update(item);
+
+    if (traverse === el) {
+      traverse = traverse.nextSibling;
+      continue;
+    }
+    if (traverse) {
+      parent.insertBefore(el, traverse);
+    } else {
+      parent.appendChild(el);
+    }
+    if (view.isMounted) {
+      view.remounted && view.remounted();
+    } else {
+      view.isMounted = true;
+      view.mounted && view.mounted();
+    }
+  }
+
+  while (traverse) {
+    var next = traverse.nextSibling;
+
+    if (key) {
+      var view = traverse.__redom_view;
+      if (view) {
+        var id = view.__id;
+        lookup[id] = null;
+      }
+    }
+    views[i++] = null;
+    parent.removeChild(traverse);
+
+    traverse = next;
+  }
+
+  views.length = data.length;
+}
+
+function text (content) {
+  return document.createTextNode(content);
+}
+
 function mount (parent, child, before) {
+  if (child == null) {
+    return;
+  }
+
   var parentEl = parent.el || parent;
   var childEl = child.el || child;
-  var wasMounted = childEl.mounted;
 
   if (childEl.nodeType) {
     if (child !== childEl) {
       childEl.view = child;
     }
-    if (wasMounted) {
-      child.remount && child.remount();
+    if (before) {
+      parentEl.insertBefore(childEl, before.el || before);
+    } else {
+      parentEl.appendChild(childEl);
     }
-    doMount(parentEl, childEl, before);
-    if (!wasMounted && (parentEl.mounted || document.contains(childEl))) {
-      childEl.mounted = true;
-      child.mount && child.mount();
-      notifyMountDown(childEl);
+    if (child.isMounted) {
+      child.remounted && child.remounted();
+    } else {
+      child.isMounted = true;
+      child.mounted && child.mounted();
     }
-    return true;
-  } else if (child.views) {
-    child.parent = parent;
-    setChildren(parentEl, child.views);
     return true;
   } else if (child.length) {
     for (var i = 0; i < child.length; i++) {
-      mount(parent, child[i], before);
+      var childEl = child.el || child;
+
+      if (child.isMounted) {
+        child.remounted && child.remounted();
+      } else {
+        child.isMounted = true;
+        child.mounted && child.mounted();
+      }
     }
     return true;
   }
   return false;
 }
 
-function unmount$1 (parent, child) {
+function unmount (parent, child) {
   var parentEl = parent.el || parent;
   var childEl = child.el || child;
 
   parentEl.removeChild(childEl);
 
-  childEl.mounted = false;
-  child.unmount && child.unmount();
-  notifyUnmountDown(childEl);
+  child.isMounted = false;
+  child.unmounted && child.unmounted();
 }
 
-function notifyMountDown (child) {
-  var traverse = child.firstChild;
+var cache$1 = {};
 
-  while (traverse) {
-    if (traverse.mounted) {
-      return;
-    }
-    traverse.mounted = true;
-    traverse.view && traverse.view.mount && traverse.view.mount();
-    notifyMountDown(traverse);
-    traverse = traverse.nextSibling;
-  }
-}
-
-function notifyUnmountDown (child) {
-  var traverse = child.firstChild;
-
-  while (traverse) {
-    if (!traverse.mounted) {
-      return;
-    }
-    traverse.mounted = false;
-    traverse.view && traverse.view.unmount && traverse.view.unmount();
-    notifyUnmountDown(traverse);
-    traverse = traverse.nextSibling;
-  }
-}
-
-function expand (source, a, b, c) {
-  var element;
-
-  if (source.nodeType) {
-    element = source.cloneNode(false);
-  } else if (typeof source === 'string') {
-    element = this.createElement(source).cloneNode(false);
-  } else if (this.allowComponents) {
-    var len = arguments.length;
-
-    switch (len) {
-      case 1: return new source();
-      case 2: return new source(a);
-      case 3: return new source(a, b);
-      case 4: return new source(a, b, c);
-    }
-
-    var args = new Array(len);
-    while (len--) args[len] = arguments[len];
-
-    return new (query.bind.apply(query, args));
-  } else {
-    throw new Error('Must pass a valid query or component!');
-  }
-
+function svg (query, a) {
+  var element = (cache$1[query] || (cache$1[query] = createElement(query))).cloneNode(false);
   var empty = true;
 
   for (var i = 1; i < arguments.length; i++) {
     var arg = arguments[i];
 
-    if (typeof arg === 'string' || typeof arg === 'number') {
-      if (empty) {
-        element.textContent = arg;
-        empty = false;
-      } else {
-        element.appendChild(document.createTextNode(arg));
-      }
-      continue;
-    }
-
-    if (typeof arg === 'function') {
+    while (typeof arg === 'function') {
       arg = arg(element);
     }
 
-    // null guard before we attempt to mount
-    if (arg == null) continue;
+    if (arg == null) {
+      continue;
+    }
 
-    if (mount(element, arg)) {
-      empty = false;
+    if (arg.nodeType) {
+      element.appendChild(arg);
+    } else if (typeof arg === 'string' || typeof arg === 'number') {
+      if (empty) {
+        element.textContent = arg;
+      } else {
+        element.appendChild(document.createTextNode(arg));
+      }
+    } else if (arg.el && arg.el.nodeType) {
+      var child = arg;
+      var childEl = arg.el;
+
+      if (child !== childEl) {
+        child.el = childEl;
+      }
+
+      if (child.isMounted) {
+        child.remounted && child.remounted();
+      } else {
+        child.mounted && child.mounted();
+      }
+
+      element.appendChild(childEl);
     } else {
-      for (var attr in arg) {
-        var value = arg[attr];
+      for (var key in arg) {
+        var value = arg[key];
 
-        if (attr === 'style') {
-          if (typeof value === 'string') {
-            element.setAttribute(attr, value);
-          } else {
-            var elementStyle = element.style;
-
-            for (var key in value) {
-              elementStyle[key] = value[key];
-            }
+        if (key === 'style' && typeof value !== 'string') {
+          for (var cssKey in value) {
+            element.style[cssKey] = value[cssKey];
           }
-        } else if (this.allowBareProps && attr in element) {
-          element[attr] = arg[attr];
         } else {
-          element.setAttribute(attr, arg[attr]);
+          element.setAttribute(key, value);
         }
       }
     }
@@ -188,201 +358,18 @@ function expand (source, a, b, c) {
   return element;
 }
 
-
-function createElement (query, svg) {
-  var cache = this.cache;
-
-  if (query in cache) return cache[query];
-
-  // query parsing magic by https://github.com/maciejhirsz
-
-  var tag, id, className;
-
-  var mode = 0;
-  var from = 0;
-
-  for (var i = 0, len = query.length; i <= len; i++) {
-    var cp = i === len ? 0 : query.charCodeAt(i);
-
-    //  cp === '#'     cp === '.'     nullterm
-    if (cp === 0x23 || cp === 0x2E || cp === 0) {
-      if (mode === 0) {
-        tag = i  === 0 ? 'div'
-            : cp === 0 ? query
-            :            query.substring(from, i);
-      } else {
-        var slice = query.substring(from, i)
-        if (mode === 1) {
-          id = slice;
-        } else if (className) {
-          className += ' ' + slice;
-        } else {
-          className = slice;
-        }
-      }
-
-      from = i + 1;
-      mode = cp === 0x23 ? 1 : 2;
-    }
-  }
-
-  var el = this.createTag(tag);
-
-  id && (el.id = id);
-  className && (el.className = className);
-
-  return cache[query] = el;
-}
-
-var domContext = {
-  cache: {},
-  expand: expand,
-  createElement: createElement,
-  allowBareProps: true,
-  allowComponents: true,
-
-  createTag: function (tag) {
-    return document.createElement(tag);
-  }
-};
-
-var el = expand.bind(domContext);
-
-el.extend = function (query) {
-  return expand.bind(domContext, domContext.createElement(query));
-}
-
-function list (View, key, initData) {
-  return new List(View, key, initData);
-}
-
-function List(View, key, initData) {
-  this.View = View;
-  this.key = key;
-  this.initData = initData;
-  this.views = [];
-
-  if (key) {
-    this.lookup = {};
-  }
-}
-
-List.prototype.update = function (data) {
-  var View = this.View;
-  var key = this.key;
-  var initData = this.initData;
-  var views = this.views;
-  var parent = this.parent;
-
-  if (key) {
-    var lookup = this.lookup;
-
-    for (var i = 0; i < data.length; i++) {
-      var item = data[i];
-      var id = typeof key === 'function' ? key(item) : item[key];
-      var view = lookup[id] || (lookup[id] = new View(initData, item, i));
-
-      view.update && view.update(item);
-
-      views[i] = view;
-      lookup[id] = view;
-    }
-    for (var i = data.length; i < views.length; i++) {
-      var id = typeof key === 'function' ? key(item) : item[key];
-
-      lookup[id] = null;
-      views[i] = null;
-    }
-  } else {
-    for (var i = 0; i < data.length; i++) {
-      var item = data[i];
-      var view = views[i] || (views[i] = new View(initData, item, i));
-      view.update && view.update(item);
-    }
-    for (var i = data.length; i < views.length; i++) {
-      views[i] = null;
-    }
-  }
-
-  views.length = data.length;
-
-  parent && setChildren(parent, views);
-}
-
-function createElementCurry (single) {
-  return function (a, b) {
-    if (arguments.length === 2) {
-      return function (el) {
-        single(el, a, b);
-      }
-    } else {
-      return function (el) {
-        for (var key in a) {
-          single(el, key, a[key]);
-        }
-      }
-    }
-  }
-}
-
-var on = createElementCurry(function (el, event, callback) {
-  el.addEventListener(event, function (e) {
-    callback.call(el.view, e);
-  });
-});
-
-var svgContext = {
-  cache: {},
-  expand: expand,
-  createElement: createElement,
-  allowBareProps: false,
-  allowComponents: false,
-
-  createTag: function (tag) {
-    return document.createElementNS('http://www.w3.org/2000/svg', tag);
-  }
-};
-
-var svg = expand.bind(svgContext);
-
 svg.extend = function (query) {
-  return expand.bind(this, svgContext.createElement(query));
-}
-
-function view (proto) {
-  return function (a, b, c, d) {
-    var view = Object.create(proto);
-    var len = arguments.length;
-
-    switch (len) {
-      case 0: proto.init.call(view); break;
-      case 1: proto.init.call(view, a); break;
-      case 2: proto.init.call(view, a, b); break;
-      case 3: proto.init.call(view, a, b, c); break;
-      
-      default:
-        var args = new Array(len);
-        var i = 0;
-        while (i < len) {
-          proto.init.apply(view, args);
-        }
-      break;
-    }
-
-    return view;
-  }
+  return svg.bind(this, query);
 }
 
 exports.el = el;
 exports.list = list;
 exports.List = List;
 exports.mount = mount;
-exports.unmount = unmount$1;
-exports.on = on;
-exports.text = text;
+exports.unmount = unmount;
 exports.setChildren = setChildren;
 exports.svg = svg;
-exports.view = view;
+exports.text = text;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
