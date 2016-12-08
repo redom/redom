ff7f4af5533ef362569f598618e99859710ff2f9
Juha Lindstedt <juha@pakastin.fi>
2016-12-08 18:29:20 +0200

    Updates

--- redom.es.js
+++ redom.es.js
@@ -77,6 +77,11 @@ function mount (parent, child, before) {
   if (child !== childEl) {
     childEl.__redom_view = child;
   }
+  if (child.isMounted) {
+    child.remount && child.remount();
+  } else {
+    child.mount && child.mount();
+  }
   if (before) {
     parentEl.insertBefore(childEl, before.el || before);
   } else {
@@ -98,6 +103,8 @@ function unmount (parent, child) {
     // try to look up the view if not provided
     child = childEl.__redom_view;
   }
+
+  child.unmount && child.unmount();
 
   parentEl.removeChild(childEl);
 
