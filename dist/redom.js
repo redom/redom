2d556e6083dbc50277c7798810d39766e03038e8
Juha Lindstedt <juha@pakastin.fi>
2017-02-02 09:46:56 +0200

    added test

--- dist/redom.js
+++ dist/redom.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.redom = global.redom || {})));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.redom = global.redom || {})));
 }(this, (function (exports) { 'use strict';
 
 var text = function (str) { return doc.createTextNode(str); };
