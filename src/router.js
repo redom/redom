d2467fbfb44d3a0c7d8d48059e1ea533aa1ab354
Juha Lindstedt <juha@pakastin.fi>
2017-02-08 15:22:12 +0200

    Router child View now gets (initData, data) in constructor, just like List

--- src/router.js
+++ src/router.js
@@ -2,25 +2,26 @@ import { el } from './el';
 import { setChildren } from './setchildren';
 import { isString } from './util';
 
-export function router (parent, Views) {
-  return new Router(parent, Views);
+export function router (parent, Views, initData) {
+  return new Router(parent, Views, initData);
 }
 
 export class Router {
-  constructor (parent, Views) {
+  constructor (parent, Views, initData) {
     this.el = isString(parent) ? el(parent) : parent;
     this.Views = Views;
+    this.initData = initData;
   }
   update (route, data) {
     if (route !== this.route) {
       const Views = this.Views;
       const View = Views[route];
 
-      this.view = View && new View();
+      this.view = View && new View(this.initData, data);
       this.route = route;
 
       setChildren(this.el, [ this.view ]);
     }
-    this.view && this.view.update && this.view.update(data);
+    this.view && this.view.update && this.view.update(data, route);
   }
 }
