a33205ff0f324d32ef97269f338e3086f6ef4c80
Juha Lindstedt <juha@pakastin.fi>
2017-03-22 23:15:00 +0200

    add ga to documentation

--- build-doc.js
+++ build-doc.js
@@ -21,6 +21,16 @@ fs.writeFileSync('dist/documentation/index.html',
     </div>
     <script src="main.js"></script>
     <script src="../prism.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-54216829-7', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>`
 );
