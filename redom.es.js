b2a3913d175e261a2e7455132e2bd3f725324825
Juha Lindstedt <juha@pakastin.fi>
2017-03-23 13:12:39 +0200

    Updates

--- redom.es.js
+++ redom.es.js
@@ -1,14 +1,8 @@
-var text = function (str) { return doc.createTextNode(str); };
-
 var hookNames = ['onmount', 'onunmount'];
 
 function mount (parent, child, before) {
-  var parentEl = parent.el || parent;
-  var childEl = child.el || child;
-
-  if (isList(childEl)) {
-    childEl = childEl.el;
-  }
+  var parentEl = getEl(parent);
+  var childEl = getEl(child);
 
   if (child === childEl && childEl.__redom_view) {
     // try to look up the view if not provided
@@ -27,7 +21,7 @@ function mount (parent, child, before) {
   }
 
   if (before) {
-    parentEl.insertBefore(childEl, before.el || before);
+    parentEl.insertBefore(childEl, getEl(before));
   } else {
     parentEl.appendChild(childEl);
   }
@@ -188,7 +182,7 @@ function trigger (childEl, eventName) {
 }
 
 function setStyle (view, arg1, arg2) {
-  var el = view.el || view;
+  var el = getEl(view);
 
   if (arguments.length > 2) {
     el.style[arg1] = arg2;
@@ -202,7 +196,7 @@ function setStyle (view, arg1, arg2) {
 }
 
 function setAttr (view, arg1, arg2) {
-  var el = view.el || view;
+  var el = getEl(view);
   var isSVG = el instanceof window.SVGElement;
 
   if (arguments.length > 2) {
@@ -222,6 +216,8 @@ function setAttr (view, arg1, arg2) {
   }
 }
 
+var text = function (str) { return doc.createTextNode(str); };
+
 function parseArguments (element, args) {
   for (var i = 0; i < args.length; i++) {
     var arg = args[i];
@@ -235,7 +231,7 @@ function parseArguments (element, args) {
       arg(element);
     } else if (isString(arg) || isNumber(arg)) {
       element.appendChild(text(arg));
-    } else if (isNode(arg) || isNode(arg.el) || isList(arg.el)) {
+    } else if (isNode(getEl(arg))) {
       mount(element, arg);
     } else if (arg.length) {
       parseArguments(element, arg);
@@ -245,12 +241,15 @@ function parseArguments (element, args) {
   }
 }
 
+var ensureEl = function (parent) { return isString(parent) ? html(parent) : getEl(parent); };
+var getEl = function (parent) { return (!parent.el && parent) || getEl(parent.el); };
+
 var isString = function (a) { return typeof a === 'string'; };
 var isNumber = function (a) { return typeof a === 'number'; };
 var isFunction = function (a) { return typeof a === 'function'; };
 
 var isNode = function (a) { return a && a.nodeType; };
-var isList = function (a) { return a && a.__redom_list; };
+
 
 var doc = document;
 
@@ -352,7 +351,7 @@ function setChildren (parent, children) {
     return setChildren(parent, [children]);
   }
 
-  var parentEl = parent.el || parent;
+  var parentEl = getEl(parent);
   var traverse = parentEl.firstChild;
 
   for (var i = 0; i < children.length; i++) {
@@ -362,11 +361,7 @@ function setChildren (parent, children) {
       continue;
     }
 
-    var childEl = child.el || child;
-
-    if (isList(childEl)) {
-      childEl = childEl.el;
-    }
+    var childEl = getEl(child);
 
     if (childEl === traverse) {
       traverse = traverse.nextSibling;
@@ -395,7 +390,7 @@ function List (parent, View, key, initData) {
   this.key = key;
   this.initData = initData;
   this.views = [];
-  this.el = getParentEl(parent);
+  this.el = ensureEl(parent);
 
   if (key) {
     this.lookup = {};
@@ -432,11 +427,11 @@ List.prototype.update = function (data) {
     } else {
       view = newViews[i] = oldViews[i] || new View(initData, item, i, data);
     }
-    var el$$1 = view.el;
-    if (el$$1.__redom_list) {
-      el$$1 = el$$1.el;
+    var el = view.el;
+    if (el.__redom_list) {
+      el = el.el;
     }
-    el$$1.__redom_view = view;
+    el.__redom_view = view;
     view.update && view.update(item, i, data);
   }
 
@@ -448,22 +443,12 @@ List.prototype.update = function (data) {
   this.views = newViews;
 };
 
-function getParentEl (parent) {
-  if (isString(parent)) {
-    return html(parent);
-  } else if (isNode(parent.el)) {
-    return parent.el;
-  } else {
-    return parent;
-  }
-}
-
 function router (parent, Views, initData) {
   return new Router(parent, Views, initData);
 }
 
 var Router = function Router (parent, Views, initData) {
-  this.el = getParentEl(parent);
+  this.el = ensureEl(parent);
   this.Views = Views;
   this.initData = initData;
 };
