b829c84f251e36ad1042eb15f62320c15eccfb80
Juha Lindstedt <juha@pakastin.fi>
2017-07-11 10:26:21 +0300

    Rebuilt

--- dist/redom.js
+++ dist/redom.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.redom = global.redom || {})));
+	(factory((global.redom = {})));
 }(this, (function (exports) { 'use strict';
 
 var HASH = '#'.charCodeAt(0);
