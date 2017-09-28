95697db10150297dcacabe78326a9d99ca2d430b
Juha Lindstedt <juha@pakastin.fi>
2017-09-28 17:46:31 +0300

    Add as code, compact ga code

--- build-doc.js
+++ build-doc.js
@@ -15,6 +15,8 @@ fs.writeFileSync('dist/documentation/index.html',
     <link rel="stylesheet" href="../prism.css">
     <link rel="stylesheet" href="https://rsms.me/inter/inter-ui.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>(adsbygoogle=window.adsbygoogle||[]).push({google_ad_client:"ca-pub-6944220562573081",enable_page_level_ads:true});</script>
   </head>
   <body>
     <div id="doc">
@@ -22,16 +24,7 @@ fs.writeFileSync('dist/documentation/index.html',
     </div>
     <script src="main.js"></script>
     <script src="../prism.js"></script>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-54216829-7', 'auto');
-      ga('send', 'pageview');
-
-    </script>
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-54216829-7', 'auto');ga('send', 'pageview');</script>
   </body>
 </html>
 `);
