d58e558b06a72baa043cbb8768e70284c2bd59d7
Juha Lindstedt <juha@pakastin.fi>
2017-10-02 01:50:18 +0300

    Add search

--- dist/documentation/main.js
+++ dist/documentation/main.js
@@ -5,7 +5,36 @@ var logoA = document.createElement('a');
 var logo = document.createElement('img');
 var hovermenu = document.createElement('div');
 var hovermenuclose = document.createElement('div');
+var searchContainer = document.createElement('div');
+var searchField = document.createElement('input');
 var mobile = false;
+var searchData = [];
+
+searchContainer.id = 'search';
+searchContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"/></svg>';
+
+searchField.autofocus = true;
+
+searchField.onclick = function (e) {
+  e.stopPropagation();
+};
+
+searchContainer.insertBefore(searchField, searchContainer.firstChild);
+menu.appendChild(searchContainer);
+
+searchField.oninput = function () {
+  var searchResults = search(searchField.value.toLowerCase());
+
+  for (var i = 0; i < menuitems.length; i++) {
+    var menuitem = menuitems[i];
+
+    if (searchResults[i]) {
+      menuitem.style.opacity = 1;
+    } else {
+      menuitem.style.opacity = 0.25;
+    }
+  }
+};
 
 hovermenu.id = 'hovermenu';
 hovermenu.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"/></svg>';
@@ -25,6 +54,7 @@ hovermenu.onclick = function (e) {
 
   document.body.style.overflow = 'hidden';
   menu.style.display = '';
+  searchField.focus();
   hovermenu.style.display = 'none';
   hovermenuclose.style.display = '';
   hovermenuclose.classList.add('slidein');
@@ -84,15 +114,34 @@ for (var i = 0; i < doc.children.length; i++) {
   if (~'H2 H3 H4'.split(' ').indexOf(child.tagName)) {
     headers.push(child);
     addItem(menu, child);
-  } else if (child.tagName === 'PRE') {
-    var code = child.querySelector('code');
+  } else if (child.tagName !== 'H1') {
+    if (child.tagName === 'PRE') {
+      var code = child.querySelector('code');
 
-    if (code.className === 'lang-html') {
-      code.className = 'language-markup';
-    } else if (code.className === 'lang-js') {
-      code.className = 'language-javascript';
+      if (code.className === 'lang-html') {
+        code.className = 'language-markup';
+      } else if (code.className === 'lang-js') {
+        code.className = 'language-javascript';
+      }
+    }
+    searchData.push({
+      header: headers.length - 1,
+      phrase: child.textContent.toLowerCase()
+    });
+  }
+}
+
+function search (str) {
+  var results = {};
+  for (var i = 0; i < searchData.length; i++) {
+    var data = searchData[i];
+    var index = data.phrase.indexOf(str);
+
+    if (~index) {
+      results[data.header] = true;
     }
   }
+  return results;
 }
 
 function addItem (menu, child) {
@@ -113,6 +162,11 @@ function addItem (menu, child) {
   item.appendChild(link);
   menucontainer.appendChild(item);
   menuitems.push(item);
+
+  item.onclick = function () {
+    searchField.value = '';
+    searchField.oninput();
+  };
 }
 menu.appendChild(menucontainer);
 document.body.appendChild(menu);
