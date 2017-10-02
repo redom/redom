ed81e2215de23cb64026cc5a99dfcfb0309fa3df
Juha Lindstedt <juha@pakastin.fi>
2017-10-02 07:47:53 +0300

    Updates

--- documentation/main.js
+++ documentation/main.js
@@ -13,8 +13,6 @@ var searchData = [];
 searchContainer.id = 'search';
 searchContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"/></svg>';
 
-searchField.autofocus = true;
-
 searchField.onclick = function (e) {
   e.stopPropagation();
 };
@@ -54,7 +52,6 @@ hovermenu.onclick = function (e) {
 
   document.body.style.overflow = 'hidden';
   menu.style.display = '';
-  searchField.focus();
   hovermenu.style.display = 'none';
   hovermenuclose.style.display = '';
   hovermenuclose.classList.add('slidein');
