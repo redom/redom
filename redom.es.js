c824e02e299867a6d3e86e3b38bbd946c0957456
Juha Lindstedt <juha@pakastin.fi>
2017-11-30 16:19:43 +0200

    Updates

--- redom.es.js
+++ redom.es.js
@@ -126,6 +126,7 @@ var doUnmount = function (child, childEl, parentEl) {
 };
 
 var hookNames = ['onmount', 'onunmount'];
+var shadowRootAvailable = 'ShadowRoot' in window;
 
 var mount = function (parent, child, before) {
   var parentEl = getEl(parent);
@@ -199,7 +200,7 @@ var doMount = function (child, childEl, parentEl, oldParent) {
       parentHooks[hook] = (parentHooks[hook] || 0) + hooks[hook];
     }
 
-    if (!triggered && (traverse === document || (parent && parent.__redom_mounted))) {
+    if (!triggered && (traverse === document || (shadowRootAvailable && (traverse instanceof window.ShadowRoot)) || (parent && parent.__redom_mounted))) {
       trigger(traverse, remount ? 'onremount' : 'onmount');
       triggered = true;
     }
