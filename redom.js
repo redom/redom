6edccdf671c6a15f3e086eff83727351de0ef667
Juha Lindstedt <juha@pakastin.fi>
2018-01-29 14:16:18 +0200

    Updates

--- redom.js
+++ redom.js
@@ -394,11 +394,6 @@ function traverse (parent, children, _current) {
       continue;
     }
 
-    if (child.length != null) {
-      current = traverse(parent, child, current);
-      continue;
-    }
-
     var childEl = getEl(child);
 
     if (childEl === current) {
@@ -406,7 +401,13 @@ function traverse (parent, children, _current) {
       continue;
     }
 
-    mount(parent, child, current);
+    if (isNode(childEl)) {
+      mount(parent, child, current);
+    }
+
+    if (child.length != null) {
+      current = traverse(parent, child, current);
+    }
   }
 
   return current;
