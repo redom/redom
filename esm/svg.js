c20e1f581c541cfbd24d38a4510fb3ce418dc1be
Juha Lindstedt <juha@pakastin.fi>
2018-07-18 21:58:17 +0300

    Fix SVG as well

--- esm/svg.js
+++ esm/svg.js
@@ -1,5 +1,5 @@
 import { createElement } from './create-element.js';
-import { parseArguments, isString, isNode, isFunction } from './util.js';
+import { parseArguments, isString, isNode, isFunction, getEl } from './util.js';
 
 const ns = 'http://www.w3.org/2000/svg';
 
@@ -21,7 +21,7 @@ export const svg = (query, ...args) => {
     throw new Error('At least one argument required');
   }
 
-  parseArguments(element, args);
+  parseArguments(getEl(element), args);
 
   return element;
 };
