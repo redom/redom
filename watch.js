bf9ae6949969a175d4e3f8ef8f10e7a378fd4265
Juha Lindstedt <juha@pakastin.fi>
2018-11-08 07:51:37 +0200

    uglify –> minify

--- watch.js
+++ watch.js
@@ -2,7 +2,7 @@ const cp = require('child_process');
 const fs = require('fs');
 
 fs.watch('esm', run('build'));
-fs.watch('dist/redom.js', () => run('uglify')); // don't do init run
+fs.watch('dist/redom.js', () => run('minify')); // don't do init run
 fs.watch('dist/doc.md', run('build-doc'));
 fs.watch('test/test.js', run('test'));
 
