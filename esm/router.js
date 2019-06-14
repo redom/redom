d688d4bdfa582484669f319b1829bfcbdaaeadae
Juha Lindstedt <juha@pakastin.fi>
2019-06-14 14:25:54 +0300

    Fix critical bug with Router

--- esm/router.js
+++ esm/router.js
@@ -20,7 +20,7 @@ export class Router {
 
       this.route = route;
 
-      if (View instanceof Node || View.el instanceof Node) {
+      if (View && (View instanceof Node || View.el instanceof Node)) {
         this.view = View;
       } else {
         this.view = View && new View(this.initData, data);
