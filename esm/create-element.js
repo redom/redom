fa8494f7efb307ca3547f9c6741fd058d48878fc
absolux <a.mouhassine@gmail.com>
2020-11-29 01:27:14 +0100

    Refactor `parseQuery()`

--- esm/create-element.js
+++ esm/create-element.js
@@ -1,12 +1,9 @@
-import { parseQuery } from './parse-query.js';
 
 export function createElement (query, ns) {
-  const { tag, id, className } = parseQuery(query);
+  const { tag, id, className } = parse(query);
   const element = ns ? document.createElementNS(ns, tag) : document.createElement(tag);
 
-  if (id) {
-    element.id = id;
-  }
+  if (id) element.id = id;
 
   if (className) {
     if (ns) {
@@ -17,4 +14,27 @@ export function createElement (query, ns) {
   }
 
   return element;
+}
+
+function parse (query) {
+  const chunks = query.split(/([.#])/);
+  let className = '';
+  let id = '';
+
+  for (let i = 1; i < chunks.length; i += 2) {
+    switch (chunks[i]) {
+      case '.':
+        className += ` ${chunks[i + 1]}`;
+        break;
+
+      case '#':
+        id = chunks[i + 1];
+    }
+  }
+
+  return {
+    className: className.trim(),
+    tag: chunks[0] || 'div',
+    id
+  };
 }
